bf0d2bac8c9ce84763d4b4f841214ac604db2055
Lenny Mwangi <mwangileny8@gmail.com>
2023-11-30 15:47:39 +0300

    refrehisng the website

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const SetupPage = () => {
+    return (
+      <div  className="p-4">
+        <Button size="default" variant="destructive"> Click Me </Button>
+      </div>
+    )
+  }
+
+  export default SetupPage;
+  
